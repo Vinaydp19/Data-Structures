dc7f054e5d4c709c3a9c60fcb7e004c97b5bc7f4
Vinaydp19 <70288679+Vinaydp19@users.noreply.github.com>
2022-07-04 14:48:03 +0530

    Create CPPTEMPLATE.cpp

---
+++ CPPTEMPLATE.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define nl "\n"
+#define pb push_back
+#define ss second 
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(0); 
+cin.tie(0);
+cout.tie(0);
+
+}
